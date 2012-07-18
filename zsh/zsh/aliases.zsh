cd5391a7e69b7349d4e8d987eb40719434f571e6
Terje Larsen <terlar@gmail.com>
2012-07-18 11:44:49 +0200

    Remove autocorrect from bundle command Signed-off-by: Terje Larsen <terlar@gmail.com>

--- zsh/zsh/aliases.zsh
+++ zsh/zsh/aliases.zsh
@@ -6,4 +6,6 @@ alias grep="grep --color"
 # copy with a progress bar
 alias cpv="rsync -poghb --backup-dir=/tmp/rsync -e /dev/null --progress --"
 
+# Ruby/Rails
 alias rc="rails c"
+alias bundle='nocorrect bundle'
