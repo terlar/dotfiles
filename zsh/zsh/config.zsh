c6013f214438ab5b4c7da7ba147adce9b1ec5caf
Terje Larsen <terlar@gmail.com>
2012-07-18 11:42:43 +0200

    Add zshenv and isolate related config
    Remove input config
    Signed-off-by: Terje Larsen <terlar@gmail.com>

--- zsh/zsh/config.zsh
+++ zsh/zsh/config.zsh
@@ -1,23 +1,3 @@
-export TERM="xterm-256color"
-
-# Locale
-export LANG="en_US.UTF-8"
-export LC_ALL="en_US.UTF-8"
-
-# Editor
-export VISUAL="vim"
-export EDITOR=$VISUAL
-export SVN_EDITOR=$VISUAL
-
-# Colors
-export CLICOLOR=1
-
-# Input
-set meta-flag on
-set input-meta on
-set output-meta on
-set convert-meta off
-
 fpath=($HOME/.zsh/functions $fpath)
 autoload -U $HOME/.zsh/functions/*(:t)
 
