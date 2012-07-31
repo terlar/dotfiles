6cd9fb2994cb86ce85ff9eaf5a6860a2e9bdf558
Terje Larsen <terlar@gmail.com>
2012-07-31 16:32:32 +0200

    Add git aliases Signed-off-by: Terje Larsen <terlar@gmail.com>

--- zsh/zsh/aliases.zsh
+++ zsh/zsh/aliases.zsh
@@ -11,6 +11,22 @@ alias mkdir="mkdir -p"
 # Copy with a progress bar
 alias cpv="rsync -poghb --backup-dir=/tmp/rsync -e /dev/null --progress --"
 
+# Git
+alias g='git'
+compdef g=git
+alias gst='git status'
+compdef _git gst=git-status
+gdv() { git diff -w "$@" | view - }
+compdef _git gdv=git-diff
+alias gc='git commit -v'
+compdef _git gc=git-commit
+alias gca='git commit -v -a'
+compdef _git gca=git-commit
+alias gco='git checkout'
+compdef _git gco=git-checkout
+alias ga='git add'
+compdef _git ga=git-add
+
 # Ruby/Rails
 alias rc="rails c"
 alias localgem_on='export BUNDLE_GEMFILE=Gemfile.local'
