4eb7eab1820c1907dd35c24e6c04c49d83ea3309
Terje Larsen <terlar@gmail.com>
2012-08-27 12:31:46 +0200

    Improve autoloaded completions

--- zsh/zsh/completion.zsh
+++ zsh/zsh/completion.zsh
@@ -52,12 +52,3 @@ bindkey . _rationalise-dot
 # Completion waiting dots
 zle -N _expand-or-complete-with-dots
 bindkey "^I" _expand-or-complete-with-dots
-
-compdef _brew brew
-compdef _bundle bundle
-compdef _gem gem
-compdef _powder powder
-compdef _rake rake
-compdef _redis-cli redis-cli
-compdef _rspec rspec
-compdef _ry ry
