27e661ac75d2ad2fba2f6fe30ac3792aecda657a
Terje Larsen <terlar@gmail.com>
2012-08-29 15:12:23 +0200

    Use cache for git status

--- zsh/zsh/prompt.zsh
+++ zsh/zsh/prompt.zsh
@@ -1,3 +1,12 @@
+# Git status
+_git_status() {
+  cat "/tmp/git-status-$$"
+}
+_git-status-tmp() {
+  $(git status --porcelain 2> /dev/null >! "/tmp/git-status-$$")
+}
+precmd_functions+=(_git-status-tmp)
+
 # Get current branch name
 _git_prompt_info() {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
@@ -5,7 +14,7 @@ _git_prompt_info() {
 }
 # Checks if working tree is dirty
 _parse_git_dirty() {
-  if [[ -n $(git status -s 2> /dev/null) ]]; then
+  if [[ -n $(_git_status) ]]; then
     echo "$GIT_PROMPT_DIRTY"
   else
     echo "$GIT_PROMPT_CLEAN"
@@ -13,7 +22,7 @@ _parse_git_dirty() {
 }
 # Get the status of the working tree
 _git_prompt_status() {
-  INDEX=$(git status --porcelain 2> /dev/null)
+  INDEX=$(_git_status)
   STATUS=""
   if $(echo "$INDEX" | grep '^?? ' &> /dev/null); then
     STATUS="$GIT_PROMPT_UNTRACKED$STATUS"
