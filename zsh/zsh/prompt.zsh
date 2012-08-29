7b072c1d56dc8967c7a6d46b57a9a5a9cd0dbdae
Terje Larsen <terlar@gmail.com>
2012-08-29 14:51:24 +0200

    Improve syntax for prompt methods

--- zsh/zsh/prompt.zsh
+++ zsh/zsh/prompt.zsh
@@ -44,22 +44,28 @@ _git_prompt_status() {
   echo $STATUS
 }
 
-_vimode_prompt_info() {
-  echo "${${KEYMAP/vicmd/$VIMODE_PROMPT_NORMAL}/(main|viins)/$VIMODE_PROMPT_INSERT}"
+_keymap_prompt() {
+  case $KEYMAP in
+    "vicmd")
+      echo $KEYMAP_PROMPT_VICMD;;
+    "main"|"viins")
+      echo $KEYMAP_PROMPT_VIINS;;
+  esac
 }
-_status_result() {
-  echo "%(?,$STATUS_RESULT_PROMPT_SUCCESS,$STATUS_RESULT_PROMPT_FAIL)"
+_exit_code_prompt() {
+  echo "%(?,$EXIT_CODE_PROMPT_SUCCESS,$EXIT_CODE_PROMPT_FAIL)"
 }
 _prompt_char() {
-  if [[ $KEYMAP == '' ]]; then
-    _status_result
+  if [ $KEYMAP ]; then
+    _keymap_prompt
   else
-    _vimode_prompt_info
+    _exit_code_prompt
   fi
 }
+
 _bundle_gemfile_prompt() {
   if [ $BUNDLE_GEMFILE ]; then
-    echo "|$(basename "$BUNDLE_GEMFILE")"
+    echo "$BUNDLE_GEMFILE_PROMPT_PREFIX${BUNDLE_GEMFILE##*/}$BUNDLE_GEMFILE_PROMPT_SUFFIX"
   fi
 }
 
@@ -78,11 +84,14 @@ GIT_PROMPT_RENAMED="%{$fg[magenta]%}➜%{$reset_color%}"
 GIT_PROMPT_UNMERGED="%{$fg[yellow]%}═%{$reset_color%}"
 GIT_PROMPT_UNTRACKED="%{$fg[cyan]%}✭%{$reset_color%}"
 
-VIMODE_PROMPT_NORMAL="%{$fg_bold[cyan]%}➤%{$reset_color%}"
-VIMODE_PROMPT_INSERT="➤"
+KEYMAP_PROMPT_VICMD="%{$fg_bold[cyan]%}➤%{$reset_color%}"
+KEYMAP_PROMPT_VIINS="➤"
 
-STATUS_RESULT_PROMPT_SUCCESS="%{$fg_bold[green]%}➤%{$reset_color%}"
-STATUS_RESULT_PROMPT_FAIL="%{$fg_bold[red]%}➤%{$reset_color%}"
+EXIT_CODE_PROMPT_SUCCESS="%{$fg_bold[green]%}➤%{$reset_color%}"
+EXIT_CODE_PROMPT_FAIL="%{$fg_bold[red]%}➤%{$reset_color%}"
+
+BUNDLE_GEMFILE_PROMPT_PREFIX="|"
+BUNDLE_GEMFILE_PROMPT_SUFFIX=""
 
 PROMPT='%{$fg[magenta]%}%n%{$reset_color%}@%{$fg[yellow]%}%m%{$reset_color%}:%{$fg[cyan]%}%0~%{$reset_color%}$(_git_prompt_info)$(_git_prompt_status)$(_bundle_gemfile_prompt)
 $(_prompt_char) '
