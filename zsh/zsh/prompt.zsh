b3fa4b71309b286b6b69c34176f205db0c517805
Terje Larsen <terlar@gmail.com>
2012-09-02 01:36:08 +0200

    Use short color-mappings

--- zsh/zsh/prompt.zsh
+++ zsh/zsh/prompt.zsh
@@ -28,27 +28,27 @@ zle -N zle-keymap-select
 
 setopt prompt_subst
 
-GIT_PROMPT_PREFIX="|%{$fg[green]%}"
-GIT_PROMPT_SUFFIX="%{$reset_color%}"
-GIT_PROMPT_DIRTY="%{$fg_bold[red]%}⚡%{$reset_color%}"
-GIT_PROMPT_AHEAD="%{$fg_bold[red]%}!%{$reset_color%}"
-GIT_PROMPT_CLEAN="%{$fg_bold[green]%}✓%{$reset_color%}"
+GIT_PROMPT_PREFIX="|%F{green}"
+GIT_PROMPT_SUFFIX="%f"
+GIT_PROMPT_DIRTY="%F{red}⚡%f"
+GIT_PROMPT_AHEAD="%F{red}!%f"
+GIT_PROMPT_CLEAN="%F{green}✓%f"
 
-GIT_PROMPT_ADDED="%{$fg[green]%}✚%{$reset_color%}"
-GIT_PROMPT_MODIFIED="%{$fg[blue]%}✹%{$reset_color%}"
-GIT_PROMPT_DELETED="%{$fg[red]%}✖%{$reset_color%}"
-GIT_PROMPT_RENAMED="%{$fg[magenta]%}➜%{$reset_color%}"
-GIT_PROMPT_UNMERGED="%{$fg[yellow]%}═%{$reset_color%}"
-GIT_PROMPT_UNTRACKED="%{$fg[cyan]%}✭%{$reset_color%}"
+GIT_PROMPT_ADDED="%F{green}✚%f"
+GIT_PROMPT_MODIFIED="%F{blue}✹%f"
+GIT_PROMPT_DELETED="%F{red}✖%f"
+GIT_PROMPT_RENAMED="%F{magenta}➜%f"
+GIT_PROMPT_UNMERGED="%F{yellow}═%f"
+GIT_PROMPT_UNTRACKED="%F{cyan}✭%f"
 
-KEYMAP_PROMPT_VICMD="%{$fg_bold[cyan]%}➤%{$reset_color%}"
+KEYMAP_PROMPT_VICMD="%F{cyan}➤%f"
 KEYMAP_PROMPT_VIINS="➤"
 
-EXIT_CODE_PROMPT_SUCCESS="%{$fg_bold[green]%}➤%{$reset_color%}"
-EXIT_CODE_PROMPT_FAIL="%{$fg_bold[red]%}➤%{$reset_color%}"
+EXIT_CODE_PROMPT_SUCCESS="%F{green}➤%f"
+EXIT_CODE_PROMPT_FAIL="%F{red}➤%f"
 
 BUNDLE_GEMFILE_PROMPT_PREFIX="|"
 BUNDLE_GEMFILE_PROMPT_SUFFIX=""
 
-PROMPT='%{$fg[magenta]%}%n%{$reset_color%}@%{$fg[yellow]%}%m%{$reset_color%}:%{$fg[cyan]%}%0~%{$reset_color%}$(_git_prompt_info)$(_git_prompt_status)$(_bundle_gemfile_prompt)
+PROMPT='%F{magenta}%n%f@%F{yellow}%m%f:%F{cyan}%0~%f$(_git_prompt_info)$(_git_prompt_status)$(_bundle_gemfile_prompt)
 $(_prompt_char) '
