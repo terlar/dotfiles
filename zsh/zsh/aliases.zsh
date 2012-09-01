0b02ec273abf11270d152e67424ba12de583d51d
Terje Larsen <terlar@gmail.com>
2012-09-02 01:37:43 +0200

    Add aliases for ls and pipes

--- zsh/zsh/aliases.zsh
+++ zsh/zsh/aliases.zsh
@@ -7,9 +7,18 @@ alias _='sudo'
 # File system
 ls --color -d . &>/dev/null 2>&1 && alias lscolor='ls --color=tty' || alias lscolor='ls -G'
 alias ls="lscolor -hF"
+alias l='ls -la'
+alias l.='ls -d .*'
+alias ll='ls -l'
+
 alias mkdir="mkdir -p"
 # Copy with a progress bar
 alias cpv="rsync -poghb --backup-dir=/tmp/rsync -e /dev/null --progress --"
+
+# Pipes
+alias -g A='| ack'
+alias -g L='| less'
+alias -g X='| xargs'
 
 # Ruby/Rails
 alias rc="rails c"
