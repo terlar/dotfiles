f9c13d0f28319d264bbbb47005f10f6b158d9144
Terje Larsen <terlar@gmail.com>
2012-10-01 17:35:22 +0200

    Fix broken t function

--- fish/config/fish/functions/t.fish
+++ fish/config/fish/functions/t.fish
@@ -1,14 +1,13 @@
 function t --description 'Attach or open new tmux session'
   # Wrapper command for OSX clipboard
-  if which reattach-to-user-namespace > /dev/null 2> /dev/null
-    set -l wrapper (reattach-to-user-namespace -l $SHELL)
+  set -l wrapper
+  if which reattach-to-user-namespace > /dev/null 2>&1
+    set wrapper "reattach-to-user-namespace -l $SHELL"
   end
 
   if test (count $argv) = 0
-    echo 'count'
     tmux attach; or tmux new $wrapper
   else
-    echo 'test'
     tmux attach -t $argv[1]; or tmux new -s $argv[1] $wrapper
   end
 end
