68aaf97a4c9140a8edd59ca9f3f438be68aaed0b
Terje Larsen <terlar@gmail.com>
2012-10-02 20:46:29 +0200

    Improve method for running aliased bundles

--- fish/config/fish/plugins/bundler/functions/__bundle_run.fish
+++ fish/config/fish/plugins/bundler/functions/__bundle_run.fish
@@ -1,17 +1,9 @@
 function __bundle_run --description 'Run file with bundler'
   if __bundler_installed
     if file_in_path Gemfile >/dev/null
-      bundle exec $argv
-      return $status
+      set argv bundle exec $argv
     end
   end
 
-  set -l cmd (which $argv[1])
-  set -e argv[1]
-
-  if test -z $cmd
-    return 1
-  end
-
-  eval $cmd $argv
+  eval command $argv
 end
