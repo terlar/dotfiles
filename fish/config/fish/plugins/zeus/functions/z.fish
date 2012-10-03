b26466522ff29ef845244305a885a7d476a2785f
Terje Larsen <terlar@gmail.com>
2012-10-03 09:53:34 +0200

    Check if zeus exist before running zeus commands

--- fish/config/fish/plugins/zeus/functions/z.fish
+++ fish/config/fish/plugins/zeus/functions/z.fish
@@ -1,4 +1,9 @@
 function z --description 'Start zeus or run command'
+  if not which zeus >/dev/null
+    echo "fish: Unknown command 'z'"
+    return 1
+  end
+
   if test (count $argv) = 0
     if not test -f zeus.json
       zeus init
