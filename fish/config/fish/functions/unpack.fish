c0dcaa4f0714b91d8ee70ed3e8a08ab21e7093ab
Terje Larsen <terlar@gmail.com>
2012-10-07 12:06:26 +0200

    Add unpack function

---
+++ fish/config/fish/functions/unpack.fish
@@ -0,0 +1,18 @@
+function unpack --description 'Unpack arbitrary archive files'
+  for file in $argv
+    switch $file
+      case '**.tar'
+        tar -xf $file
+      case '**.tar.gz' '**.tgz'
+        tar -zxf $file
+      case '**.tar.bz' '**.tar.bz2' '**.tbz' '**.tbz2'
+        tar -jxf $file
+      case '**.rar'
+        unrar e $file
+      case '**.zip'
+        unzip $file
+      case '**'
+        echo File $file is of unknown type
+    end
+  end
+end
