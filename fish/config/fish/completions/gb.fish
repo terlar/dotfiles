25702fb62f165420e738c7c1b9e75f83f87ab7bf
Terje Larsen <terlar@gmail.com>
2012-10-16 02:16:52 +0200

    Add aliased git completions

---
+++ fish/config/fish/completions/gb.fish
@@ -0,0 +1,10 @@
+function __fish_git_branches
+  git branch --no-color -a 2>/dev/null | sed 's/^..//'
+end
+
+complete -f -c gb -a '(__fish_git_branches)' -d 'Branch'
+complete -f -c gb -s d -d 'Delete Branch'
+complete -f -c gb -s D -d 'Force deletion of branch'
+complete -f -c gb -s m -d 'Rename branch'
+complete -f -c gb -s M -d 'Force renaming branch'
+complete -f -c gb -s a -d 'Lists both local and remote branches'
---
+++ fish/config/fish/completions/gco.fish
@@ -0,0 +1,12 @@
+function __fish_git_branches
+  git branch --no-color -a 2>/dev/null | sed 's/^..//'
+end
+
+function __fish_git_tags
+  git tag
+end
+
+complete -f -c gco -a '(__fish_git_branches)' --description 'Branch'
+complete -f -c gco -a '(__fish_git_tags)' --description 'Tag'
+complete -f -c gco -s b -d 'Create a new branch'
+complete -f -c gco -s t -l track -d 'Track branch'
