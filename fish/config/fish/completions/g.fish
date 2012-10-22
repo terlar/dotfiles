03b0ec85222707d938a9aa1132ebf97843a5791e
Terje Larsen <terlar@gmail.com>
2012-10-22 21:03:06 +0200

    Allow more characters for git alias completion

--- fish/config/fish/completions/g.fish
+++ fish/config/fish/completions/g.fish
@@ -1,5 +1,5 @@
 function __fish_git_aliases
-  git config --get-regexp alias | sed 's/^alias.\([a-z]*\).*/\1/'
+  git config --get-regexp alias | sed "s/^alias\.\([^ ]*\).*/\1/"
 end
 
 complete -f -c g -a '(__fish_git_aliases)' -d 'Alias'
