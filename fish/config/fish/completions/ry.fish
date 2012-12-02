8963c5bddba84986bf09fb7cc4daac3923cfa6d5
Terje Larsen <terlar@gmail.com>
2012-12-02 17:34:23 +0100

    Add support for ruby_build in ry completion

--- fish/config/fish/completions/ry.fish
+++ fish/config/fish/completions/ry.fish
@@ -20,13 +20,19 @@ function __fish_ry_rubies
   ry ls
 end
 
+function __fish_ry_ruby_build
+  ruby-build --definitions
+end
+
 complete -f -c ry -n '__fish_ry_needs_command' -a 'ls' -d 'Output the installed rubies'
 complete -f -c ry -n '__fish_ry_needs_command' -a 'rubies' -d 'Output the installed rubies, and highlight the current one'
 
-complete -f -c ry -a '(__fish_ry_rubies)' -d 'ruby'
+complete -f -c ry -a '(__fish_ry_rubies)' -d 'Ruby'
 complete -f -c ry -n '__fish_ry_needs_command' -a 'use' -d 'Use the given ruby'
 complete -f -c ry -n '__fish_ry_needs_command' -a 'rm remove' -d 'Remove the given ruby'
+
 complete -f -c ry -n '__fish_ry_needs_command' -a 'install' -d 'Download, compile and install'
+complete -f -c ry -n '__fish_ry_using_command install' -a '(__fish_ry_ruby_build)' -d 'Ruby'
 
 complete -f -c ry -n '__fish_ry_needs_command' -a 'exec' -d 'Execute in the context of ruby'
 complete -f -c ry -n '__fish_ry_needs_command' -a 'binpath' -d 'Print the bin directory for the given ruby'
