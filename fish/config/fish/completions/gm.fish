49d00e042283193ba2808001237be3581a24b0c4
Terje Larsen <terlar@gmail.com>
2013-01-16 11:32:17 +0100

    Fix mistake in gm completion

--- fish/config/fish/completions/gm.fish
+++ fish/config/fish/completions/gm.fish
@@ -6,9 +6,9 @@ complete -f -c gm -a '(__fish_git_branches)' -d 'Branch'
 
 complete -f -c gm -l abort -d 'Abort the current conflict resolution process, and try to reconstruct the pre-merge state'
 
-complete -f -c gr -l log -d 'In addition to branch names, populate the log message with one-line descriptions'
-complete -f -c gr -l no-log -d 'Do not list one-line descriptions from the actual commits being merged'
-complete -f -c gr -l no-ff -d 'Create a merge commit even when the merge resolves as a fast-forward'
+complete -f -c gm -l log -d 'In addition to branch names, populate the log message with one-line descriptions'
+complete -f -c gm -l no-log -d 'Do not list one-line descriptions from the actual commits being merged'
+complete -f -c gm -l no-ff -d 'Create a merge commit even when the merge resolves as a fast-forward'
 
-complete -f -c gr -l squash -d 'Create a single commit on top of the current branch whose effect is the same as merging another branch'
-complete -f -c gr -l no-squash -d 'Perform the merge and commit the result'
+complete -f -c gm -l squash -d 'Create a single commit on top of the current branch whose effect is the same as merging another branch'
+complete -f -c gm -l no-squash -d 'Perform the merge and commit the result'
