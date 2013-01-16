bcfc21bae81af3034cb59ae4ae308ec9d0a23386
Terje Larsen <terlar@gmail.com>
2013-01-16 12:06:18 +0100

    Add option to skip xip generation

--- fish/config/fish/completions/kc.fish
+++ fish/config/fish/completions/kc.fish
@@ -2,3 +2,5 @@
 
 complete -f -c kc -a update -d 'Updates the database'
 complete -f -c kc -a export -d 'Exports the database'
+
+complete -f -c kc -l no-xip -d 'Do not generate xip domains'
--- fish/config/fish/functions/kviberg-config.fish
+++ fish/config/fish/functions/kviberg-config.fish
@@ -27,22 +27,33 @@ function kviberg-config-export
 end
 
 function kviberg-config-update
+  set -l xip 1
+
+  for i in $argv
+    switch $i
+      case --no-xip
+        set -e xip
+    end
+  end
+
   echo '==> Recreating database'
   rake db:drop db:create db:schema:load >/dev/null; ok; or return 1
   echo '==> Populating database'
   mysql -uroot kviberg-config-development < db/export.sql; ok; or return 1
 
-  echo '==> Inserting debug domains'
-  set -l ip_address (ifconfig | grep 'inet ' | grep -v 127.0.0.1 | cut -d ' ' -f 2)
-  if test -n "$ip_address"
-    for site in 1:dekho 2:bikroy 3:ikman 4:tonaton
-      set -l parts (echo $site | tr ':' "\n")
-      mysql -uroot kviberg-config-development\
-            -e "insert into domains (site_id, domain_name) values ($parts[1], '$parts[2].kviberg.$ip_address.xip.io');"\
-            -e "insert into domains (site_id, domain_name) values ($parts[1], '$parts[2].kviberg-mobile.$ip_address.xip.io');"
+  if set -q xip
+    echo '==> Inserting debug domains'
+    set -l ip_address (ifconfig | grep 'inet ' | grep -v 127.0.0.1 | cut -d ' ' -f 2)
+    if test -n "$ip_address"
+      for site in 1:dekho 2:bikroy 3:ikman 4:tonaton
+        set -l parts (echo $site | tr ':' "\n")
+        mysql -uroot kviberg-config-development\
+              -e "insert into domains (site_id, domain_name) values ($parts[1], '$parts[2].kviberg.$ip_address.xip.io');"\
+              -e "insert into domains (site_id, domain_name) values ($parts[1], '$parts[2].kviberg-mobile.$ip_address.xip.io');"
+      end
     end
+    ok
   end
-  ok
 
   echo '==> Flushing redis'
   redis-cli flushall
