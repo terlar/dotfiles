0bdb51411df6ffe53268af6f110c8c4d843761dd
Terje Larsen <terlar@gmail.com>
2013-01-16 11:30:50 +0100

    Update git alias completions

--- fish/config/fish/completions/gr.fish
+++ fish/config/fish/completions/gr.fish
@@ -3,3 +3,10 @@ function __fish_git_branches
 end
 
 complete -f -c gr -a '(__fish_git_branches)' -d 'Branch'
+complete -f -c gr -s i -l interactive -d 'Interactive mode'
+
+complete -f -c gr -l continue -d 'Restart the rebasing process after having resolved a merge conflict'
+complete -f -c gr -l abort -d 'Abort the rebase operation and reset HEAD to the original branch'
+complete -f -c gr -l skip -d 'Restart the rebasing process by skipping the current patch'
+
+complete -f -c gr -l no-ff -d 'No fast-forward'
