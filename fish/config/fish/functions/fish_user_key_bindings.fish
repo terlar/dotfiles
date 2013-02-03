dee75ec3eeeb516471fcdf0c53813d9898468373
Terje Larsen <terlar@gmail.com>
2013-02-03 01:35:57 +0100

    Add binding for insering last argument of previous command

--- fish/config/fish/functions/fish_user_key_bindings.fish
+++ fish/config/fish/functions/fish_user_key_bindings.fish
@@ -2,6 +2,9 @@ function fish_user_key_bindings
   # Accept or go to end of line
   bind \ce accept-autosuggestion
 
+  # Insert last argument of previous command
+  bind \e. history-token-search-backward
+
   bind \e! __runsudo
 end
 
