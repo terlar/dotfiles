f9bae2b6ca3b0f7ebe272062f5f3b532d31f1b79
Terje Larsen <terlar@gmail.com>
2013-02-14 09:13:39 +0100

    Fix coding style for powder completions

--- fish/config/fish/completions/powder.fish
+++ fish/config/fish/completions/powder.fish
@@ -1,7 +1,7 @@
 # fish completion for powder
 function __fish_powder
-  powder help | grep powder | \
-    sed 's/^  powder //; s/[[:blank:]].*#/'(printf '\t')'/g'
+  powder help | grep powder \
+    | sed 's/^  powder //; s/[[:blank:]].*#/'(printf '\t')'/g'
 end
 
-complete -f -c powder -a '(__fish_powder)' -d 'Task'
+complete -f -c powder -a '(__fish_powder)'
