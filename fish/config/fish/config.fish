854fa2b05edf0e3756192cb7edeae2a272021b03
Terje Larsen <terlar@gmail.com>
2013-03-01 09:18:49 +0100

    Add less colors for man pages

--- fish/config/fish/config.fish
+++ fish/config/fish/config.fish
@@ -22,6 +22,16 @@ set -gx NODE_PATH /usr/local/lib/node_modules
 set -gx REMOTE_GEM_CACHE_PATH $HOME/.remote-gem-cache
 
 
+# Less colors
+set -x LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
+set -x LESS_TERMCAP_md \e'[01;38;5;74m'  # begin bold
+set -x LESS_TERMCAP_me \e'[0m'           # end mode
+set -x LESS_TERMCAP_se \e'[0m'           # end standout-mode
+set -x LESS_TERMCAP_so \e'[38;5;246m;'   # begin standout-mode - info box
+set -x LESS_TERMCAP_ue \e'[0m'           # end underline
+set -x LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
+
+
 # Alias
 function !      ; sudo $argv ; end
 function rc     ; rails console ; end
