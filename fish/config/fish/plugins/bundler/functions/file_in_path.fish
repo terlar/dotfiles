c8d5c144571e152774698f40d4579f2cb61a0904
Terje Larsen <terlar@gmail.com>
2013-03-14 09:48:42 +0100

    Update variable name in file_in_path

--- fish/config/fish/plugins/bundler/functions/file_in_path.fish
+++ fish/config/fish/plugins/bundler/functions/file_in_path.fish
@@ -1,13 +1,13 @@
 function file_in_path --description 'Find file in path'
   set -l file $argv[1]
-  set -l check_dir $PWD
+  set -l dir $PWD
 
-  while test $check_dir != '/'
-    if test -f $check_dir/$file
-      echo -n $check_dir/$file
+  while test $dir != '/'
+    if test -f $dir/$file
+      echo -n $dir/$file
       return 0
     end
-    set check_dir (dirname $check_dir)
+    set dir (dirname $dir)
   end
 
   return 1
