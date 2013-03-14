b854e5ad6d88bc7888df56f3f09502a50e53c45a
Terje Larsen <terlar@gmail.com>
2013-03-14 09:55:21 +0100

    Add version for fry

--- fish/config/fish/plugins/fry/functions/fry-help.fish
+++ fish/config/fish/plugins/fry/functions/fry-help.fish
@@ -6,5 +6,6 @@ function fry-help --description 'Display help information'
   echo '  rubies      List the installed rubies with the current one highlighted'
   echo '  current     Show the current ruby'
   echo '  use <name>  Use the ruby given by <name>'
+  echo '  version     Display the current version of fry'
   echo '  help        Display help information'
 end
