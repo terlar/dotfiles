5881aa84af9457726ba3e3070aa2cb6c0793adff
Terje Larsen <terlar@gmail.com>
2013-03-14 09:45:52 +0100

    Handle .ruby-version file anywhere in path

--- fish/config/fish/plugins/fry/fry.fish
+++ fish/config/fish/plugins/fry/fry.fish
@@ -6,7 +6,9 @@ set PATH $fry_rubies/current/bin $PATH
 function __fry_auto_switch --on-variable PWD --description 'Auto-switch ruby version from .ruby-version file'
   status --is-command-substitution; and return
   set -q fry_auto_switch; or return
-  test -f .ruby-version; or return
 
-  fry use (cat .ruby-version)
+  set -l version_file (file_in_path .ruby-version)
+  test -z "$version_file"; and return
+
+  fry use (cat $version_file)
 end
---
+++ fish/config/fish/plugins/fry/functions/file_in_path.fish
@@ -0,0 +1,14 @@
+function file_in_path --description 'Find file in path'
+  set -l file $argv[1]
+  set -l dir $PWD
+
+  while test $dir != '/'
+    if test -f $dir/$file
+      echo -n $dir/$file
+      return 0
+    end
+    set dir (dirname $dir)
+  end
+
+  return 1
+end
