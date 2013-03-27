9e7f439a57976edca4d3eb692fc15dae3b0806b3
Terje Larsen <terlar@gmail.com>
2013-03-27 15:17:14 +0100

    Fix broken ls color wrapper

--- fish/config/fish/functions/ls.fish
+++ fish/config/fish/functions/ls.fish
@@ -1,7 +1,7 @@
 function ls --description 'List files with style'
   set argv -h $argv
 
-  if command ls --color -d . ^/dev/null
+  if test (command ls --color -d . ^/dev/null)
     set argv --color=tty $argv
   else
     set argv -G $argv
