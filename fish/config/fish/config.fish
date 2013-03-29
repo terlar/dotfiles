44e4404ac4849633fcdd40038fa0a26a75af3789
Terje Larsen <terlar@gmail.com>
2013-03-29 10:57:19 +0100

    Fix less termcap

--- fish/config/fish/config.fish
+++ fish/config/fish/config.fish
@@ -18,10 +18,10 @@ set -gx REMOTE_GEM_CACHE_PATH $HOME/.remote-gem-cache
 set -x LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
 set -x LESS_TERMCAP_md \e'[01;38;5;74m'  # begin bold
 set -x LESS_TERMCAP_me \e'[0m'           # end mode
+set -x LESS_TERMCAP_so \e'[38;5;246m'    # begin standout-mode - info box
 set -x LESS_TERMCAP_se \e'[0m'           # end standout-mode
-set -x LESS_TERMCAP_so \e'[38;5;246m;'   # begin standout-mode - info box
-set -x LESS_TERMCAP_ue \e'[0m'           # end underline
 set -x LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
+set -x LESS_TERMCAP_ue \e'[0m'           # end underline
 
 
 # Alias
