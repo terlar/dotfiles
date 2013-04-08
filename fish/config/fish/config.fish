ee7fee872ccafcd4401a635d45841ddd8d3677fa
Terje Larsen <terlar@gmail.com>
2013-04-08 15:57:24 +0200

    Add command to binstub for spring

--- fish/config/fish/config.fish
+++ fish/config/fish/config.fish
@@ -1,21 +1,23 @@
-set fish_greeting
+if status --is-login
+  set fish_greeting
 
-set -gx TERM xterm-256color
-set -gx EDITOR vim
-set -gx LC_ALL en_US.UTF-8
+  set -gx TERM xterm-256color
+  set -gx EDITOR vim
+  set -gx LC_ALL en_US.UTF-8
 
-# Less colors
-set -x LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
-set -x LESS_TERMCAP_md \e'[01;38;5;74m'  # begin bold
-set -x LESS_TERMCAP_me \e'[0m'           # end mode
-set -x LESS_TERMCAP_so \e'[38;5;246m'    # begin standout-mode - info box
-set -x LESS_TERMCAP_se \e'[0m'           # end standout-mode
-set -x LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
-set -x LESS_TERMCAP_ue \e'[0m'           # end underline
+  # Less colors
+  set -x LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
+  set -x LESS_TERMCAP_md \e'[01;38;5;74m'  # begin bold
+  set -x LESS_TERMCAP_me \e'[0m'           # end mode
+  set -x LESS_TERMCAP_so \e'[38;5;246m'    # begin standout-mode - info box
+  set -x LESS_TERMCAP_se \e'[0m'           # end standout-mode
+  set -x LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
+  set -x LESS_TERMCAP_ue \e'[0m'           # end underline
 
-# Paths
-set PATH $HOME/.local/bin /usr/local/bin $PATH
-set -gx NODE_PATH /usr/local/lib/node_modules
+  # Paths
+  set PATH $HOME/.local/bin /usr/local/bin $PATH
+  set -gx NODE_PATH /usr/local/lib/node_modules
+end
 
 
 # Plugins
@@ -69,6 +71,7 @@ function gr   ; git rebase $argv ; end
 function rc ; rails console $argv ; end
 function rg ; rails generate $argv ; end
 function s  ; spring $argv; end
+function sb ; for i in rspec rake rails; spring binstub $i; end; end
 
 # Tools
 function kc ; kviberg-config $argv ; end
