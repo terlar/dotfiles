c466f554054a0927fe7922a3464340404c0c8591
Terje Larsen <terlar@gmail.com>
2013-05-10 16:51:26 +0200

    Remove reattach-to-user-namespace logic

--- fish/config/fish/functions/t.fish
+++ fish/config/fish/functions/t.fish
@@ -1,15 +1,9 @@
 function t --description 'Attach or open new tmux session'
-  # Wrapper command for OSX clipboard
-  set -l wrapper
-  if which reattach-to-user-namespace >/dev/null
-    set wrapper "reattach-to-user-namespace -l $SHELL"
-  end
-
   if test (count $argv) -gt 0
     tmux attach -t $argv[1]
-    or tmux new -s $argv[1] $wrapper
+    or tmux new -s $argv[1]
   else
     tmux attach
-    or tmux new $wrapper
+    or tmux new
   end
 end
