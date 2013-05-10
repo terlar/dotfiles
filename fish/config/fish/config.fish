e4d73686da6e40c76e6d6fc56aee6f6506074aa9
Terje Larsen <terlar@gmail.com>
2013-05-10 18:31:55 +0200

    Fix environment variables
    They need to be set even if it is not login

--- fish/config/fish/config.fish
+++ fish/config/fish/config.fish
@@ -1,23 +1,21 @@
-if status --is-login
-  set fish_greeting
+set fish_greeting
 
-  set -x TERM xterm-256color
-  set -x EDITOR vim
-  set -x LC_ALL en_US.UTF-8
+set -x TERM xterm-256color
+set -x EDITOR vim
+set -x LC_ALL en_US.UTF-8
 
-  # Less colors
-  set -x LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
-  set -x LESS_TERMCAP_md \e'[01;38;5;74m'  # begin bold
-  set -x LESS_TERMCAP_me \e'[0m'           # end mode
-  set -x LESS_TERMCAP_so \e'[38;5;246m'    # begin standout-mode - info box
-  set -x LESS_TERMCAP_se \e'[0m'           # end standout-mode
-  set -x LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
-  set -x LESS_TERMCAP_ue \e'[0m'           # end underline
+# Less colors
+set -x LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
+set -x LESS_TERMCAP_md \e'[01;38;5;74m'  # begin bold
+set -x LESS_TERMCAP_me \e'[0m'           # end mode
+set -x LESS_TERMCAP_so \e'[38;5;246m'    # begin standout-mode - info box
+set -x LESS_TERMCAP_se \e'[0m'           # end standout-mode
+set -x LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
+set -x LESS_TERMCAP_ue \e'[0m'           # end underline
 
-  # Paths
-  set PATH $HOME/.local/bin /usr/local/bin $PATH
-  set -x NODE_PATH /usr/local/lib/node_modules
-end
+# Paths
+set PATH $HOME/.local/bin /usr/local/bin $PATH
+set -x NODE_PATH /usr/local/lib/node_modules
 
 
 # Plugins
