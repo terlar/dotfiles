0c1b21784dfd3e04f4196ef8a539bfdf7c9e3616
Terje Larsen <terlar@gmail.com>
2013-05-20 12:33:07 +0200

    Add ability to erase abbreviations

--- fish/config/fish/functions/abbreviate.fish
+++ fish/config/fish/functions/abbreviate.fish
@@ -1,3 +1,14 @@
-function abbreviate --description 'Creates an abbreviation'
+function abbreviate --description 'Define a new abbreviation'
+  if test (count $argv) -lt 2
+    echo 'abbreviate: Takes two arguments. First abbreviation and then expanded command'
+    return 1
+  end
+
+  if abbreviations -q $argv[1]
+    echo "abbreviate: Abbreviation '$argv[1]' already defined."
+    return 1
+  end
+
   set -U fish_abbreviations $fish_abbreviations "$argv"
+  return 0
 end
--- fish/config/fish/functions/abbreviations.fish
+++ fish/config/fish/functions/abbreviations.fish
@@ -4,12 +4,16 @@ function abbreviations --description 'List, show and query abbreviations'
     return
   end
 
+  set -l abbreviation_index 0
   set -l expanded_abbreviation
 
   for i in $fish_abbreviations
+    set abbreviation_index (math $abbreviation_index + 1)
     echo $i | read -l abbreviation command
+
     if test $abbreviation = $argv[-1]
       set expanded_abbreviation $command
+      break
     end
   end
 
@@ -17,6 +21,8 @@ function abbreviations --description 'List, show and query abbreviations'
     switch $argv[1]
       case -q --query
         return 0
+      case -e --erase
+        set -e fish_abbreviations[$abbreviation_index]
       case '*'
         echo $expanded_abbreviation
       end
