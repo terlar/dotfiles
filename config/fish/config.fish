332a47f888c356fb169e43095b5d0566eb5cfaf7
Terje Larsen <terlar@gmail.com>
2013-07-07 13:37:01 +0200

    Fix path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -14,23 +14,23 @@ set -x LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
 set -x LESS_TERMCAP_ue \e'[0m'           # end underline
 
 # Prompt colors
-set -q fish_color_user; or set -U fish_color_user magenta
-set -q fish_color_host; or set -U fish_color_host yellow
-set -q fish_color_cwd; or set -U fish_color_cwd green
-set -q fish_pager_color_description; or set -U fish_pager_color_description 555 yellow
+set -q fish_color_user              ; or set -U fish_color_user magenta
+set -q fish_color_host              ; or set -U fish_color_host yellow
+set -q fish_color_cwd               ; or set -U fish_color_cwd green
+set -q fish_pager_color_description ; or set -U fish_pager_color_description 555 yellow
 
 # Paths
-# Hack to remove existing paths
-set __fish_added_user_paths /usr/local/bin
+# Hack to make sure /usr/local/bin is prepended
+test $PATH[-1] = /usr/local/bin; and set -e PATH[-1]
 set fish_user_paths $HOME/.local/bin /usr/local/bin
 set -x NODE_PATH /usr/local/lib/node_modules
 
 
 # Plugins
 set -l plugins_path (dirname (status -f))/plugins
+
 # Bundler
 . $plugins_path/bundler/bundler.fish
-
 # Farm
 . /usr/local/share/fish-farm/farm.fish
 # Fry
