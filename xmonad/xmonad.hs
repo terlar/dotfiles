0d12eecb78dcf8ef4ebd4ac21b05c13323ef984e
Terje Larsen <terlar@gmail.com>
2013-11-02 18:39:21 +0100

    Update xmonad config
    - Organize keys in their own section.
    - Add keys for screenshots

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -2,7 +2,7 @@
 import XMonad
 import System.IO
 
--- Utilities
+-- Util
 import XMonad.Util.EZConfig
 
 -- Hooks
@@ -16,27 +16,23 @@ import XMonad.Actions.CycleRecentWS
 -- Layouts
 import XMonad.Layout.NoBorders
 
-main = do
-  xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig
-    { modMask            = mod4Mask
-    , normalBorderColor  = "#dddddd"
-    , focusedBorderColor = "#3d4962"
-    , terminal           = "termite"
-    , manageHook = composeAll
-      [ isFullscreen                --> doFullFloat
-      , isDialog                    --> doCenterFloat
-      , className =? "feh"          --> doCenterFloat
-      , className =? "Pavucontrol"  --> doCenterFloat
-      , manageHook defaultConfig
-      ]
-    , layoutHook = smartBorders $ layoutHook defaultConfig
-    } `additionalKeysP`
-    [ ("M-p", spawn "x=$(yeganesh -x -- $DMENU_OPTIONS) && exec $x"   ) -- Launcher
-    , ("M-q", spawn "xmonad --recompile && xmonad --restart"          ) -- Restart xmonad
+--
+-- Keybindings
+--
+myKeys :: [(String, X())]
+myKeys =
+    -- Window Manager Keys
+    [ ("M-q", spawn "xmonad --recompile && xmonad --restart"          ) -- Restart XMonad
     , ("M-<Tab>", cycleRecentWS [xK_Super_L] xK_Tab xK_Tab            ) -- Cycle recent WS
     , ("M-u", focusUrgent                                             ) -- Focus urgent WS
-    , ("M-<Esc>" , spawn "i3lock -i ~/Pictures/saltside.png -c 000000") -- Lock screen
+
+    -- Launch Keys
+    , ("M-p", spawn "x=$(yeganesh -x -- $DMENU_OPTIONS) && exec $x"   ) -- Launcher
+
     -- Media Keys
+    , ("M-<Esc>", spawn "i3lock -i ~/Pictures/saltside.png -c 000000" ) -- Lock screen
+    , ("M-`"                    , spawn "scrot"                       ) -- Screenshot
+    , ("M-S-`"                  , spawn "sleep 0.2; scrot -s"         ) -- Partial screenshot
     , ("<XF86MonBrightnessUp>"  , spawn "xbacklight -inc 10"          ) -- Brighness up
     , ("<XF86MonBrightnessDown>", spawn "xbacklight -dec 10"          ) -- Brighness down
     , ("<XF86AudioPlay>"        , spawn "ncmpcpp toggle"              ) -- Play/Pause track
@@ -47,3 +43,27 @@ main = do
     , ("<XF86AudioRaiseVolume>" , spawn "amixer -q set Master 5%+"    ) -- Increase volume
     , ("<XF86AudioMute>"        , spawn "amixer -q set Master toggle" ) -- Mute volume
     ]
+
+
+--
+-- Main
+--
+main :: IO ()
+main = xmonad
+     $ withUrgencyHook NoUrgencyHook
+     $ defaultConfig
+        { modMask            = mod4Mask
+        , normalBorderColor  = "#dddddd"
+        , focusedBorderColor = "#3d4962"
+        , terminal           = "termite"
+        , manageHook         = composeAll
+            [ isFullscreen                --> doFullFloat
+            , isDialog                    --> doCenterFloat
+            , className =? "feh"          --> doCenterFloat
+            , className =? "Pavucontrol"  --> doCenterFloat
+            , className =? "Skype"        --> doShift "9"
+            , manageHook defaultConfig
+            ]
+        , layoutHook = smartBorders $ layoutHook defaultConfig
+        }
+        `additionalKeysP` myKeys
