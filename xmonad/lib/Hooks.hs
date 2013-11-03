c5c7a2f98053e3b9158bec90f2c89995341de8a0
Terje Larsen <terlar@gmail.com>
2013-11-03 17:52:46 +0100

    Improve hooks

--- xmonad/lib/Hooks.hs
+++ xmonad/lib/Hooks.hs
@@ -7,14 +7,19 @@ import XMonad
 import XMonad.ManageHook
 import XMonad.Hooks.ManageHelpers
 
+import XMonad.Actions.CopyWindow
+
 import qualified XMonad.StackSet as W
+
+import Topics
 
 myManageHook :: ManageHook
 myManageHook =
     (composeAll . concat $
-    [ [ isFullscreen                --> doFullFloat ]
-    , [ isDialog                    --> doCenterFloat ]
-    , [ className =? x              --> doCenterFloat | x <- floats ]
+    [ [ isFullscreen          --> doFullFloat ]
+    , [ isDialog              --> doCenterFloat ]
+    , [ className =? "Zenity" --> ask >>= doF . W.sink ]
+    , [ className =? x        --> doCenterFloat | x <- floats ]
     ])
     where
       floats = ["feh", "Pavucontrol"]
@@ -25,5 +30,8 @@ myManageHook =
       doSideFloat' p = doSideFloat p <+> doMaster
       doRectFloat' r = doRectFloat r <+> doMaster
       doFullFloat' = doFullFloat <+> doMaster
+      doShiftAndGo ws = doF (W.greedyView ws) <+> doShift ws
+      doCopyToAll = ask >>= doF . \w -> (\ws -> foldr($) ws (map (copyWindow w) myTopics))
+      doCenterFloatToAll = doCopyToAll <+> doCenterFloat'
 
 myToggleWS = windows $ W.view =<< W.tag . head . filter ((\x -> x /= "NSP") . W.tag) . W.hidden
