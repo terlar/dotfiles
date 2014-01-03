37f7432c6ce6cea83d5881a7833b2bbc2057c326
Terje Larsen <terlar@gmail.com>
2014-01-03 09:30:23 +0100

    Remove xip

--- config/fish/functions/kviberg-config.fish
+++ config/fish/functions/kviberg-config.fish
@@ -27,12 +27,8 @@ function kviberg-config-export
 end
 
 function kviberg-config-import
-  set -l xip 1
-
   for i in $argv
     switch $i
-      case --no-xip
-        set -e xip
     end
   end
 
@@ -41,24 +37,10 @@ function kviberg-config-import
   echo '==> Populating database'
   mysql -uroot kviberg-config-development < db/export.sql; ok; or return 1
 
-  if set -q xip
-    echo '==> Inserting debug domains'
-    set -l ip_address (ifconfig | grep 'inet ' | grep -v 127.0.0.1 | cut -d ' ' -f 2)
-    if test -n "$ip_address"
-      for site in 1:dekho 2:bikroy 3:ikman 4:tonaton
-        set -l parts (echo $site | tr ':' "\n")
-        mysql -uroot kviberg-config-development\
-              -e "insert into domains (site_id, domain_name) values ($parts[1], '$parts[2].kviberg.$ip_address.xip.io');"\
-              -e "insert into domains (site_id, domain_name) values ($parts[1], '$parts[2].kviberg-mobile.$ip_address.xip.io');"
-      end
-    end
-    ok
-  end
-
   echo '==> Flushing redis'
   redis-cli flushall
   echo '==> Flushing memcached'
-  echo 'flush_all' | nc -c 127.0.0.1 11211
+  echo 'flush_all' | nc -c 127.0.0.1 11211; ok; or return 1
 end
 
 function ok
