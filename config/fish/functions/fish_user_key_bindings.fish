1cd9d33534a4a5e2d684f04e3542c0f1b1d5980c
Terje Larsen <terlar@gmail.com>
2014-02-05 20:47:35 +0100

    fish: fix code style in key bindings

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -57,28 +57,32 @@ function __fish_eval_token
 end
 
 function __commandline_stash -d 'Stash current command line'
-    set -g __stash_command_position (commandline -C)
-    set -g __stash_command (commandline -b)
-    commandline -r ""
+  set -g __stash_command_position (commandline -C)
+  set -g __stash_command (commandline -b)
+  commandline -r ''
 end
 
 function __commandline_pop -d 'Pop last stashed command line'
-    if not set -q __stash_command
-        return
-    end
-    commandline -r $__stash_command
-    if set -q __stash_command_position
-        commandline -C $__stash_command_position
-    end
-    set -e __stash_command
-    set -e __stash_command_position
+  if not set -q __stash_command
+    return
+  end
+
+  commandline -r $__stash_command
+
+  if set -q __stash_command_position
+    commandline -C $__stash_command_position
+  end
+
+  set -e __stash_command
+  set -e __stash_command_position
 end
 
 function __commandline_toggle -d 'Stash current commandline if not empty, otherwise pop last stashed commandline'
-    set -l cmd (commandline -b)
-    if test "$cmd"
-        __commandline_stash
-    else
-        __commandline_pop
-    end
+  set -l cmd (commandline -b)
+
+  if test "$cmd"
+    __commandline_stash
+  else
+    __commandline_pop
+  end
 end
