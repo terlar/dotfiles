3315f7e4eb0e5f5a4fbe58dd34164a7cc3b76d70
Terje Larsen <terlar@gmail.com>
2014-05-23 21:06:10 +0200

    offlineimap: add configuration
    Use pass to get mail credentials

---
+++ offlineimap.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import os, re, subprocess
+
+mapping = { 'INBOX':              'inbox'
+          , '[Gmail]/All Mail':   'archive'
+          , '[Gmail]/Drafts':     'drafts'
+          , '[Gmail]/Important':  'important'
+          , '[Gmail]/Sent Mail':  'sent'
+          , '[Gmail]/Spam':       'spam'
+          , '[Gmail]/Starred':    'starred'
+          , '[Gmail]/Trash':      'trash'
+          , '[Gmail]/Economy':    'economy'
+          }
+
+r_mapping = { val: key for key, val in mapping.items() }
+
+def nt_remote(folder):
+    return mapping.get(folder, folder)
+
+def nt_local(folder):
+    return r_mapping.get(folder, folder)
+
+# folderfilter = exclude(['Label', 'Label', ... ])
+def exclude(excludes):
+    return lambda folder: not folder in excludes
+
+def get_credentials(account, meta=None):
+    out = subprocess.check_output(['pass', 'show', 'Mail/%s' % account], universal_newlines=True)
+    lines = str(out).strip().splitlines()
+    if meta is None:
+        return lines[0]
+    else:
+        regex = re.compile('^%s: (.*)' % meta)
+        matches = (m.group(1) for l in lines for m in [regex.search(l)] if m)
+        return next(matches, None)
