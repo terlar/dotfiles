4e8265298890011c268596f4eab70938e973981f
Terje Larsen <terlar@gmail.com>
2014-08-04 17:15:10 +0200

    fish: update right prompt to work with new CMD_DURATION implementation

--- config/fish/functions/fish_right_prompt.fish
+++ config/fish/functions/fish_right_prompt.fish
@@ -1,7 +1,8 @@
 function fish_right_prompt --description 'Write out the prompt'
-  if set -q CMD_DURATION
-    set_color white
-    echo -n "($CMD_DURATION) "
-    set_color normal
-  end
+  set -q CMD_DURATION; or return
+  test $CMD_DURATION -ge 100; or return
+
+  set_color white
+  printf '(%gs) ' (math "scale=3; $CMD_DURATION / 1000")
+  set_color normal
 end
