dd29fbd9e571c574598f7ebf4a4d08f4773a7987
Terje Larsen <terlar@gmail.com>
2014-10-03 08:35:51 +0200

    fish: repurpose e to launch editor

--- config/fish/functions/e.fish
+++ config/fish/functions/e.fish
@@ -1,16 +1,3 @@
-function e --description 'Execute with environment'
-	set -l vars
-
-	for i in $argv
-		switch $i
-			case '*=*'
-				echo $i | sed 's|=| |' | read -l var value
-				set vars $vars "set -lx $var $value;"
-				set -e argv[1]
-			case '*'
-				break
-		end
-	end
-
-	eval "begin; $vars $argv; end"
+function e --description 'Execute $EDITOR with argv'
+	eval "$EDITOR $argv"
 end
