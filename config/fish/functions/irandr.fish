8e09a27200f0bbc44a4cabf8b147e3d707e3876a
Terje Larsen <terlar@gmail.com>
2014-11-03 13:01:40 +0100

    irandr: disable dead screens

--- config/fish/functions/irandr.fish
+++ config/fish/functions/irandr.fish
@@ -12,14 +12,14 @@ function irandr --description 'Interactive xrander configuration'
 		set -l position
 
 		switch (echo $rotation | tr '[:upper:]' '[:lower:]')
-			case l left
-				set rotation left
-			case r right
-				set rotation right
-			case i inverted
-				set rotation inverted
-			case n normal ''
-				set rotation normal
+		case l left
+			set rotation left
+		case r right
+			set rotation right
+		case i inverted
+			set rotation inverted
+		case n normal ''
+			set rotation normal
 		end
 
 		if test -n "$previous"
@@ -29,5 +29,12 @@ function irandr --description 'Interactive xrander configuration'
 		echo "xrandr --output $screen --auto --rotate $rotation$position" | source
 
 		set -l previous $screen
+	end
+
+	set -l dead_screens (xrandr | grep 'disconnected .* (' | sed 's/^\([A-Za-z0-9\-]*\).*/\1/')
+
+	for screen in $dead_screens
+		echo Disable $screen
+		xrandr --output $screen --off
 	end
 end
