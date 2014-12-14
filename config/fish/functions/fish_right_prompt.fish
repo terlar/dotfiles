797b0012febcad10856ee0c1d9861f6a8d4ca4d5
Terje Larsen <terlar@gmail.com>
2014-12-14 08:33:59 +0100

    fish: update right prompt cmd duration format

--- config/fish/functions/fish_right_prompt.fish
+++ config/fish/functions/fish_right_prompt.fish
@@ -19,7 +19,7 @@ function fish_right_prompt --description 'Write out the prompt'
 	test $CMD_DURATION -ge 100; or return
 
 	set_color white
-	printf ' (%gs) ' (math "scale=3; $CMD_DURATION / 1000")
+	printf " (%'.2fs) " (echo $CMD_DURATION | sed 's/.\{3\}$/.&/')
 
 	set_color normal
 end
