017436bc903396f93728d2e18ae0a73dc6e22121
Terje Larsen <terlar@gmail.com>
2015-01-02 12:34:35 +0100

    fish: bundler plugin path position
    Make sure the bundler path is before the current ruby path.

--- config/fish/plugins/bundler/functions/__bundle_set_binstub_path.fish
+++ config/fish/plugins/bundler/functions/__bundle_set_binstub_path.fish
@@ -4,9 +4,18 @@ function __bundle_set_binstub_path --description 'Add binstub to path'
 	end
 
 	set -l gemfile_path $argv[1]
+	set -l binstub_path (dirname $gemfile_path)/bin
 
-	set -xg BUNDLE_BINSTUB_PATH (dirname $gemfile_path)/bin
-	if test -d $BUNDLE_BINSTUB_PATH
-		set fish_user_paths $fish_user_paths $BUNDLE_BINSTUB_PATH
+	if test -d $binstub_path
+		set -xg BUNDLE_BINSTUB_PATH $binstub_path
+
+		set -l ruby_path (dirname (type -p ruby))
+		set -l pos (contains -i $ruby_path $fish_user_paths)
+		if test $status -eq 0
+			set fish_user_paths $fish_user_paths[1..$pos $pos..-1]
+			set fish_user_paths[$pos] $binstub_path
+		else
+			set fish_user_paths $fish_user_paths $binstub_path
+		end
 	end
 end
