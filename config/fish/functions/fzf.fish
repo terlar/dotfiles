3e1034c2c05fe8204c1907d4853841083d35e03f
Terje Larsen <terlar@gmail.com>
2015-08-05 10:06:50 +0200

    fish: remove fzf functions

--- config/fish/functions/fzf.fish
+++
@@ -1,9 +0,0 @@
-function fzf
-	if test -z (git rev-parse HEAD ^/dev/null)
-		set -eg FZF_DEFAULT_COMMAND
-	else
-		set -gx FZF_DEFAULT_COMMAND 'git ls-tree -r --name-only HEAD'
-	end
-
-	~/.fzf/bin/fzf $argv
-end
