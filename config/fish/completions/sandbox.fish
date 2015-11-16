fa70dff67b8b04532bc9fb6282329c2377240b22
Terje Larsen <terlar@gmail.com>
2015-11-16 11:20:10 +0100

    fish: completions for sandbox

---
+++ config/fish/completions/sandbox.fish
@@ -0,0 +1,28 @@
+function __fish_sandbox_needs_command
+	set cmd (commandline -opc)
+	if [ (count $cmd) -eq 1 -a $cmd[1] = 'sandbox' ]
+		return 0
+	end
+	return 1
+end
+
+function __fish_sandbox_using_command
+	set cmd (commandline -opc)
+	if [ (count $cmd) -gt 1 ]
+		if [ $argv[1] = $cmd[2] ]
+			return 0
+		end
+	end
+	return 1
+end
+
+function __fish_sandbox_commands
+	sandbox help | grep '  ' | string trim | string replace -r ' +' \t
+end
+
+function __fish_sandbox_overrides
+	sandbox override | grep '-' | string replace ' - ' ''
+end
+
+complete -f -c sandbox -n '__fish_sandbox_needs_command' -a '(__fish_sandbox_commands)'
+complete -f -c sandbox -n '__fish_sandbox_using_command override' -a '(__fish_sandbox_overrides)' -d 'Service'
