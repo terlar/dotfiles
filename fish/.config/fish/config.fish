33ec185d27325a560244cd852f3d1cd1c3703f9d
Terje Larsen <terlar@gmail.com>
2016-04-17 08:06:01 +0200

    fish: remove manual script loading
    These scripts should add themselves during installation.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,14 +1,4 @@
 set -Uq fish_setup_done; or fish_setup
 
-# Scripts
-set -l script_files \
-	/usr/local/share/fish-farm/farm.fish \
-	/usr/local/share/fry/fry.fish
-
-for file in $script_files
-	test -f $file; or continue
-	source $file
-end
-
 # Environment
 source $HOME/.env.fish
