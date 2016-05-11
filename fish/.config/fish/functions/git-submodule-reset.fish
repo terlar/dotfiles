fdfdaa8ab04f3beaf78bea679238d8bd03d9115c
Terje Larsen <terlar@gmail.com>
2016-05-11 08:13:19 +0200

    fish: git submodule reset function
    This is a bit tedious operation to do manually every time.

---
+++ fish/.config/fish/functions/git-submodule-reset.fish
@@ -0,0 +1,4 @@
+function git-submodule-reset --description 'Reset all changes to submodules'
+	git submodule foreach --recursive 'git reset --hard; git clean -fd'
+	git submodule update --init --recursive
+end
