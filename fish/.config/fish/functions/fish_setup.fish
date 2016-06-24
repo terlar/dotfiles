8ffb26a05b632f97434fcbad22a9f7f26a7a9254
Terje Larsen <terlar@gmail.com>
2016-06-24 05:32:54 +0200

    fish: remove less config

--- fish/.config/fish/functions/fish_setup.fish
+++ fish/.config/fish/functions/fish_setup.fish
@@ -21,18 +21,9 @@ function fish_setup --description 'Setup fish variables'
     # Enable VI mode
     set -U fish_key_bindings fish_vi_key_bindings
 
-    # Less colors
-    set -Ux LESS_TERMCAP_mb \e'[01;31m' # begin blinking
-    set -Ux LESS_TERMCAP_md \e'[01;38;5;75m' # begin bold
-    set -Ux LESS_TERMCAP_so \e'[01;44m' # begin standout-mode - info box
-    set -Ux LESS_TERMCAP_us \e'[04;38;5;200m' # begin underline
-    set -Ux LESS_TERMCAP_me \e'[0m' # end mode
-    set -Ux LESS_TERMCAP_se \e'[0m' # end standout-mode
-    set -Ux LESS_TERMCAP_ue \e'[0m' # end underline
-
     # FZF Defaults
     set -Ux FZF_DEFAULT_OPTS --no-256
-    set -Ux FZF_DEFAULT_COMMAND 'ag -l -g ""'
+    set -Ux FZF_DEFAULT_COMMAND 'ag --hidden -l -g ""'
 
     # Colors
     fish_load_colors
