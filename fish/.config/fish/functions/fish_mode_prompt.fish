86fd72b5fd7d8aa7ebdb9f8ba8eda612ae362379
Terje Larsen <terlar@gmail.com>
2016-07-25 08:52:35 +0200

    fish: improve vim mode prompt

--- fish/.config/fish/functions/fish_mode_prompt.fish
+++ fish/.config/fish/functions/fish_mode_prompt.fish
@@ -4,14 +4,18 @@ function fish_mode_prompt --description 'Displays the current mode'
         switch $fish_bind_mode
             case default
                 set_color red
+                echo -n '🅽'
             case insert
                 set_color green
+                echo -n '🅸'
             case replace-one
                 set_color green
+                echo -n '🆁'
             case visual
                 set_color magenta
+                echo -n '🆅'
         end
-        echo -n '■'
+        echo -n ' '
         set_color normal
     end
 end
