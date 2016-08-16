4b5760ee0b3612849ad5e6ecd0eaf1d8a4c72428
Terje Larsen <terlar@gmail.com>
2016-08-16 09:56:13 +0200

    fish: extract duration printing

--- fish/.config/fish/functions/fish_right_prompt.fish
+++ fish/.config/fish/functions/fish_right_prompt.fish
@@ -3,22 +3,7 @@ function fish_right_prompt --description 'Write out the prompt'
 
     if set -q CMD_DURATION
         set_color grey
-        if test $CMD_DURATION -gt 60000
-            set total_seconds (math $CMD_DURATION / 1000)
-            if test $total_seconds -gt 3600
-                set hours (math $total_seconds / 3600)
-                set remainder (math $total_seconds '%' 3600)
-                set minutes (math $remainder / 60)
-                set seconds (math $remainder '%' 60)
-                printf '(%dh%dm%ds)' $hours $minutes $seconds
-            else if test $total_seconds -gt 60
-                set minutes (math $total_seconds / 60)
-                set seconds (math $total_seconds '%' 60)
-                printf '(%dm%ds)' $minutes $seconds
-            end
-        else if test $CMD_DURATION -ge 100
-            printf "(%'.2fs) " (string replace -r '(\d?)(\d{3})$' '$1.$2' $CMD_DURATION)
-        end
+        print_duration $CMD_DURATION
         set_color normal
     end
 
