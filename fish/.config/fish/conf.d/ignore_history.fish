865929d294de423710f304207ec87a7acef24995
Terje Larsen <terlar@gmail.com>
2016-10-27 17:40:04 +0200

    fish: update ingore history hook

--- fish/.config/fish/conf.d/ignore_history.fish
+++ fish/.config/fish/conf.d/ignore_history.fish
@@ -1,3 +1,4 @@
 function __commandline_ignore_history --on-event fish_preexec
-    builtin history --delete fg bg
+    set -l ignored_terms fg bg
+    history --delete --exact --case-sensitive $ignored_terms
 end
