49c636f6a299a8404e30ed41cacbe31cf9ea9623
Terje Larsen <terlar@gmail.com>
2016-11-13 18:24:11 +0100

    emacs: update package archive if empty
    not only in the case of use-package not being installed

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -55,9 +55,10 @@
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
 
 (unless (package-installed-p 'use-package)
-  (package-refresh-contents)
   (package-install 'use-package))
 (require 'use-package)
 (setq use-package-always-ensure t)
