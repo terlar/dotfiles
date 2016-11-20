76a7a8b95156fb72c5563b616bcae903dc78b298
Terje Larsen <terlar@gmail.com>
2016-11-20 14:32:50 +0100

    emacs: explain and add line-completion to hippie expand

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -494,14 +494,25 @@ KEY must be given in `kbd' notation."
   :bind ([remap dabbrev-expand] . hippie-expand)
   :config
   (setq hippie-expand-try-functions-list
-        '(try-expand-dabbrev
+        '(
+          ;; Try to expand word "dynamically", searching the current buffer.
+          try-expand-dabbrev
+          ;; Try to expand word "dynamically", searching all other buffers.
           try-expand-dabbrev-all-buffers
+          ;; Try to expand word "dynamically", searching the kill ring.
           try-expand-dabbrev-from-kill
+          ;; Try to complete text as a file name, as many characters as unique.
           try-complete-file-name-partially
+          ;; Try to complete text as a file name.
           try-complete-file-name
+          ;; Try to expand word before point according to all abbrev tables.
           try-expand-all-abbrevs
+          ;; Try to complete the current line to an entire line in the buffer.
           try-expand-list
+          try-expand-line
+          ;; Try to complete as an Emacs Lisp symbol, as many characters as unique.
           try-complete-lisp-symbol-partially
+          ;; Try to complete word as an Emacs Lisp symbol.
           try-complete-lisp-symbol)))
 
 ;; Highlight current line
