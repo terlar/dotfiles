cdc905829463f452a47a88d65139b6779c3a2ef5
Terje Larsen <terlar@gmail.com>
2016-11-29 10:59:05 +0100

    fish: remove unreachable case
    With `-n 1` you have to enter at least one character so therefore you
    cannot reach the default case.

--- fish/.config/fish/functions/read_confirm.fish
+++ fish/.config/fish/functions/read_confirm.fish
@@ -1,6 +1,5 @@
 function read_confirm
     set -l prompt 'Do you want to continue?'
-    set -l default 1
 
     for pair in (options $argv)
         echo $pair | read -l option value
@@ -8,21 +7,17 @@ function read_confirm
         switch $option
             case p prompt
                 set prompt $value
-            case d default
-                set default $value
         end
     end
 
     while true
-        read -l -n 1 -p "echo '$prompt [Y/n] '" confirm
+        read -l -n 1 -p "echo '$prompt [y/n] '" confirm
 
         switch $confirm
             case Y y
                 return 0
             case N n
                 return 1
-            case ''
-                return $default
         end
     end
 end
