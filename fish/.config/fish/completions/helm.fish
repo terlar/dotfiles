805878b43d1009dcd85b395109a61f3e1d11def8
Terje Larsen <terlar@gmail.com>
2017-02-17 13:30:38 +0100

    fish: update helm completion for 2.2.0
    More robust field selection, by being header based instead of column
    number based.

--- fish/.config/fish/completions/helm.fish
+++ fish/.config/fish/completions/helm.fish
@@ -19,6 +19,30 @@ function __helm_using_command
     test "$argv" = "$found"
 end
 
+function __helm_grab_headers
+    set -l lines
+    while read -l line
+        if set -q header
+            set lines $lines $line
+        else
+            set header $line
+        end
+    end
+
+    set -l positions
+    for name in $argv
+        set positions $positions (string match -rn -- $name $header | string match -r '\S+')
+    end
+
+    for line in $lines
+        set -l values
+        for pos in $positions
+            set values $values (string sub -s $pos -- $line | string match -r '\S+')
+        end
+        echo $values
+    end
+end
+
 function __helm_seen_any_subcommand_from -a cmd
     __fish_seen_subcommand_from (__helm_subcommands $cmd | string replace -r '\t.*$' '')
 end
@@ -26,6 +50,7 @@ end
 function __helm_subcommands -a cmd
     switch $cmd
         case ''
+            echo completion\t'Generate bash autocompletions script'
             echo create\t'Create a new chart with the given name'
             echo delete\t'Delete the release from Kubernetes'
             echo dependency\t"Manage a chart's dependencies"
@@ -40,10 +65,12 @@ function __helm_subcommands -a cmd
             echo list\t'List releases'
             echo package\t'Package a chart directory into a chart archive'
             echo repo\t'Operate on chart repositories'
+            echo reset\t'Uninstalls Tiller from a cluster'
             echo rollback\t'Roll back a release to a previous revision'
             echo search\t'Search for a keyword in charts'
             echo serve\t'Start a local http web server'
             echo status\t'Display the status of the named release'
+            echo test\t'Test a release'
             echo upgrade\t'Upgrade a release'
             echo verify\t'Verify that a chart has been signed and is valid'
             echo version\t'Print the client/server version information'
@@ -80,13 +107,16 @@ function __helm_releases
 end
 
 function __helm_release_completions
-    helm ls ^/dev/null | awk 'NR >= 2 { print $1"\tRelease of "$NF  }'
+    for release in (helm ls ^/dev/null | __helm_grab_headers NAME CHART)
+        echo $release | read -l name chart
+        echo $name\t"Release of $chart"
+    end
 end
 
 function __helm_release_revisions
     set -l cmd (commandline -poc)
 
-    for pair in (helm ls | awk 'NR >= 2 { print $1" "$2 }')
+    for pair in (helm ls ^/dev/null | __helm_grab_headers NAME REVISION)
         echo $pair | read -l release revision
 
         if contains $release $cmd
@@ -97,17 +127,17 @@ function __helm_release_revisions
 end
 
 function __helm_repositories
-    helm repo list | awk 'NR >= 2 { print $1 }'
+    helm repo list ^/dev/null | __helm_grab_headers NAME
 end
 
 function __helm_charts
-    helm search | awk 'NR >= 2 && !/^local\// { print $1 }'
+    helm search ^/dev/null | string match -v 'local/*' | __helm_grab_headers NAME
 end
 
 function __helm_chart_versions
     set -l cmd (commandline -poc)
 
-    for pair in (helm search -l | awk 'NR >= 2 { print $1" "$2 }')
+    for pair in (helm search -l ^/dev/null | __helm_grab_headers NAME VERSION)
         echo $pair | read -l chart version
 
         if contains $chart $cmd
@@ -123,6 +153,7 @@ complete -c helm -l debug -f -d 'Enable verbose output'
 complete -c helm -l home -r -d 'Location of your Helm config'
 complete -c helm -l host -x -d 'Address of tiller'
 complete -c helm -l kube-context -x -a '(__helm_kube_contexts)' -d 'Name of the kubeconfig context to use'
+complete -c helm -l tiller-namespace -x -d 'Namespace of tiller'
 complete -c helm -s h -l help -f -d 'More information about a command'
 
 #
@@ -141,6 +172,7 @@ complete -c helm -n '__helm_using_command delete' -f -a '(__helm_release_complet
 complete -c helm -n '__helm_using_command delete' -l dry-run -f -d 'Simulate a delete'
 complete -c helm -n '__helm_using_command delete' -l no-hooks -f -d 'Prevent hooks from running during deletion'
 complete -c helm -n '__helm_using_command delete' -l purge -f -d 'Remove the release from the store'
+complete -c helm -n '__helm_using_command delete' -l timeout -x -d 'Timeout for kubernetes operations'
 
 # helm dependency [command]
 complete -c helm -n '__helm_using_command dependency; and not __helm_seen_any_subcommand_from dependency' -x -a '(__helm_subcommands dependency)'
@@ -185,6 +217,7 @@ complete -c helm -n '__helm_using_command init' -l canary-image -f -d 'Use the c
 complete -c helm -n '__helm_using_command init' -s c -l client-only -f -d 'Do not install tiller'
 complete -c helm -n '__helm_using_command init' -l dry-run -f -d 'Do not install local or remote'
 complete -c helm -n '__helm_using_command init' -s i -l tiller-image -x -d 'Override tiller image'
+complete -c helm -n '__helm_using_command init' -l upgrade -f -d 'Upgrade if tiller is already installed'
 
 # helm inspect [command]
 complete -c helm -n '__helm_using_command inspect; and not __helm_seen_any_subcommand_from inspect' -f -a '(__helm_subcommands inspect)'
@@ -207,9 +240,11 @@ complete -c helm -n '__helm_using_command install' -l namespace -x -a '(__helm_k
 complete -c helm -n '__helm_using_command install' -l no-hooks -f -d 'Prevent hooks from running during install'
 complete -c helm -n '__helm_using_command install' -l replace -f -d 'Re-use the given name if already used'
 complete -c helm -n '__helm_using_command install' -l set -x -d 'Set values on the command line'
+complete -c helm -n '__helm_using_command install' -l timeout -x -d 'Timeout for kubernetes operations'
 complete -c helm -n '__helm_using_command install' -s f -l values -r -d 'Specify values in a YAML file'
 complete -c helm -n '__helm_using_command install' -l verify -f -d 'Verify the package before installing it'
 complete -c helm -n '__helm_using_command install' -l version -x -a '(__helm_chart_versions)' -d 'Chart version'
+complete -c helm -n '__helm_using_command install' -l wait -f -d 'Wait until all resources to be ready'
 
 # helm lint [flags] PATH
 complete -c helm -n '__helm_using_command lint' -l strict -f -d 'Fail on lint warnings'
@@ -222,6 +257,7 @@ complete -c helm -n '__helm_using_command list' -l deleting -f -d 'Show releases
 complete -c helm -n '__helm_using_command list' -l deployed -f -d 'Show deployed releases'
 complete -c helm -n '__helm_using_command list' -l failed -f -d 'Show failed releases'
 complete -c helm -n '__helm_using_command list' -s m -l max -x -d 'Maximum number of releases to fetch'
+complete -c helm -n '__helm_using_command list' -l namespace -x -a '(__helm_kube_namespaces)' -d 'Show releases within a specific namespace'
 complete -c helm -n '__helm_using_command list' -s o -l offset -x -a '(__helm_release_completions)' -d 'Next release name in the list'
 complete -c helm -n '__helm_using_command list' -s r -l reverse -f -d 'Reverse the sort order'
 complete -c helm -n '__helm_using_command list' -s q -l short -f -d 'Output short listing format'
@@ -231,11 +267,15 @@ complete -c helm -n '__helm_using_command package' -l key -x -d 'Name of the key
 complete -c helm -n '__helm_using_command package' -l keyring -r -d 'Keyring containing public keys'
 complete -c helm -n '__helm_using_command package' -l save -f -d 'Save packaged chart to local chart repository'
 complete -c helm -n '__helm_using_command package' -l sign -f -d 'Use a PGP private key to sign this package'
+complete -c helm -n '__helm_using_command package' -l version -x -d 'Set the version on the chart to this semver version'
 
 # helm repo [command]
 complete -c helm -n '__helm_using_command repo; and not __helm_seen_any_subcommand_from repo' -f -a '(__helm_subcommands repo)'
 
 # helm repo add [flags] [NAME] [URL]
+complete -c helm -n '__helm_using_command repo add' -l ca-file -r -d 'CA bundle'
+complete -c helm -n '__helm_using_command repo add' -l cert-file -r -d 'Identify HTTPS client using this SSL certificate file'
+complete -c helm -n '__helm_using_command repo add' -l key-file -r -d 'Identify HTTPS client using this SSL key file'
 complete -c helm -n '__helm_using_command repo add' -l no-update -f -d 'Raise error if repo is already registered'
 
 # helm repo index [flags] [DIR]
@@ -245,12 +285,19 @@ complete -c helm -n '__helm_using_command repo index' -l url -x -d 'URL of chart
 # helm repo remove [flags] [NAME]
 complete -c helm -n '__helm_using_command repo remove' -f -a '(__helm_repositories)' -d 'Repository'
 
+# helm reset [flags]
+complete -c helm -n '__helm_using_command reset' -s f -l force -f -d 'Uninstall even if there are releases installed'
+complete -c helm -n '__helm_using_command reset' -l remove-helm-home -f -d 'If set deletes $HELM_HOME'
+
 # helm rollback [RELEASE] [REVISION] [flags]
 complete -c helm -n '__helm_using_command rollback; and not __fish_seen_subcommand_from (__helm_releases)' -f -a '(__helm_release_completions)' -d 'Release'
 complete -c helm -n '__helm_using_command rollback' -f -a '(__helm_release_revisions)' -d 'Revision'
 
 complete -c helm -n '__helm_using_command rollback' -l dry-run -f -d 'Simulate a rollback'
 complete -c helm -n '__helm_using_command rollback' -l no-hooks -f -d 'Prevent hooks from running during rollback'
+complete -c helm -n '__helm_using_command rollback' -l recreate-pods -f -d 'Performs pods restart for the resource if applicable'
+complete -c helm -n '__helm_using_command rollback' -l timeout -x -d 'Timeout for kubernetes operations'
+complete -c helm -n '__helm_using_command rollback' -l wait -f -d 'Wait until all resources to be ready'
 
 # helm search [keyword] [flags]
 complete -c helm -n '__helm_using_command search' -s r -l regexp -f -d 'Use regular expressions for searching'
@@ -265,6 +312,12 @@ complete -c helm -n '__helm_using_command status' -f -a '(__helm_release_complet
 
 complete -c helm -n '__helm_using_command status' -l revision -x -a '(__helm_release_revisions)' -d 'Revision'
 
+# helm test RELEASE [flags]
+complete -c helm -n '__helm_using_command test' -f -a '(__helm_release_completions)' -d 'Release'
+
+complete -c helm -n '__helm_using_command test' -l cleanup -f -d 'Delete test pods upon completion'
+complete -c helm -n '__helm_using_command test' -s t -l timeout -f -d 'Timeout for kubernetes operations'
+
 # helm upgrade [RELEASE] [CHART] [flags]
 complete -c helm -n '__helm_using_command upgrade; and not __fish_seen_subcommand_from (__helm_releases)' -f -a '(__helm_release_completions)' -d 'Release'
 complete -c helm -n '__helm_using_command upgrade; and __fish_seen_subcommand_from (__helm_releases); and not __fish_seen_subcommand_from (__helm_charts)' -a '(__helm_charts)' -d 'Chart'
@@ -274,10 +327,14 @@ complete -c helm -n '__helm_using_command upgrade' -s i -l install -f -d "Run an
 complete -c helm -n '__helm_using_command upgrade' -l keyring -r -d 'Keyring containing public keys'
 complete -c helm -n '__helm_using_command upgrade' -l namespace -x -a '(__helm_kube_namespaces)' -d 'Namespace'
 complete -c helm -n '__helm_using_command upgrade' -l no-hooks -f -d 'Disable pre/post upgrade hooks'
+complete -c helm -n '__helm_using_command upgrade' -l recreate-pods -f -d 'Performs pods restart for the resource if applicable'
+complete -c helm -n '__helm_using_command upgrade' -l reset-values -f -d 'Reset the values to the ones built into the chart'
 complete -c helm -n '__helm_using_command upgrade' -l set -x -d 'Set values on the command line'
+complete -c helm -n '__helm_using_command upgrade' -l timeout -f -d 'Timeout for kubernetes operations'
 complete -c helm -n '__helm_using_command upgrade' -s f -l values -r -d 'Specify values in a YAML file'
 complete -c helm -n '__helm_using_command upgrade' -l verify -f -d 'Verify the provenance of the chart before upgrading'
 complete -c helm -n '__helm_using_command upgrade' -l version -x -a '(__helm_chart_versions)' -d 'Chart version'
+complete -c helm -n '__helm_using_command upgrade' -l wait -f -d 'Wait until all resources to be ready'
 
 # helm verify [flags] PATH
 complete -c helm -n '__helm_using_command verify' -l keyring -r -d 'Keyring containing public keys'
@@ -285,3 +342,4 @@ complete -c helm -n '__helm_using_command verify' -l keyring -r -d 'Keyring cont
 # helm version [flags]
 complete -c helm -n '__helm_using_command version' -s c -l client -f -d 'Show the client version'
 complete -c helm -n '__helm_using_command version' -s s -l server -f -d 'Show the server version'
+complete -c helm -n '__helm_using_command version' -l short -f -d 'Print the version number'
