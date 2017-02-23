daf35a6e2034740d581cf7035cfa0ef85c9b1190
Terje Larsen <terlar@gmail.com>
2017-02-23 21:47:29 +0100

    emacs: update rotate words
    Include more true/false and remove bad functioning vim bindings. This
    conflicts with the default - binding.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -625,14 +625,10 @@ KEY must be given in `kbd' notation."
   (("C-c <" . rotate-text-backward)
    ("C-c >" . rotate-text))
   :defer t
-  :init
-  (progn
-    (with-eval-after-load 'evil
-      (evil-define-key 'normal prog-mode-map
-        (kbd "-") #'rotate-text
-        (kbd "_") #'rotate-text-backward)))
   :config
-  (add-to-list 'rotate-text-words '("assert" "refute")))
+  (dolist (item '(("assert" "refute")
+                  ("true" "false")))
+    (add-to-list 'rotate-text-words item)))
 
 ;; Show tab width configuration in mode-line
 (use-package show-tab-width-mode
