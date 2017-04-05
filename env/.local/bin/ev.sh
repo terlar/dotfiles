ffb941790e3fb4742fda4106c2da2681aea78efe
Terje Larsen <terlar@gmail.com>
2017-04-05 07:22:30 +0200

    env: update ev.sh to handle extra args as env evaluation

--- env/.local/bin/ev.sh
+++ env/.local/bin/ev.sh
@@ -6,12 +6,12 @@ set -o pipefail
 EVPATH="${EVPATH:-$HOME/.config/env}"
 
 print_env() {
-	rel_varpath="${1}"
-	abs_varpath="${EVPATH}/${rel_varpath}"
+	local rel_varpath="${1}"
+	local abs_varpath="${EVPATH}/${rel_varpath}"
 
 	if [ -f "${abs_varpath}" ]
 	then
-		varname="${rel_varpath##*/}"
+		local varname="${rel_varpath##*/}"
 		if [ -x "${abs_varpath}" ]
 		then
 			echo "${varname}"="$(${abs_varpath})"
@@ -26,7 +26,7 @@ print_env() {
 		done
 	else
 		>&2 echo "ev: ${rel_varpath} is not a file or directory in EVPATH"
-		exit 1
+		return 1
 	fi
 }
 
@@ -34,9 +34,22 @@ main() {
 	if [ ! -d "${EVPATH}" ]
 	then
 		>&2 echo "ev: EVPATH '${EVPATH}' is not a directory"
-		exit 1
+		return 1
 	fi
-	print_env "${1?DIR or FILE not provided}"
+
+	local target="${1?DIR or FILE not provided}"
+	shift 1
+
+	if [ $# -eq 0 ]
+	then
+		print_env "${target}"
+	else
+		# We want to split each env var	to prevent everything
+		# being set into the first variable name.
+		#
+		# shellcheck disable=SC2046
+		env $(print_env "${target}") "$@"
+	fi
 }
 
 main "$@"
