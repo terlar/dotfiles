ca90f82c93dcaa5ec87ca0d07bf98a302e08aa5d
Terje Larsen <terlar@gmail.com>
2017-08-14 10:14:02 +0200

    fish: group prompt_context functionality
    This is a prompt context displayed on directory change instead of always showing
    the prompt context on every command.

--- fish/.config/fish/conf.d/prompt_context.fish
+++ fish/.config/fish/conf.d/prompt_context.fish
@@ -1,3 +1,56 @@
+function __prompt_context_update --on-event fish_prompt
+    set -l prompt_context_new (__prompt_context_build)
+    if not test "$prompt_context_new" = "$__prompt_context_current"
+        set -g __prompt_context_current $prompt_context_new
+    end
+end
+
 function __prompt_context_reload -v __prompt_context_current
     echo $__prompt_context_current
+end
+
+function __prompt_context_build
+    set prompt ' '
+    set git_dir (command git rev-parse --show-toplevel ^/dev/null)
+
+    set strong_color (set_color black --bold)
+    set normal_color (set_color normal)
+    set subtle_color (set_color white)
+
+    if set -q TMUX
+        set strong_color '#[fg=black,bold]'
+        set normal_color '#[default]'
+        set subtle_color '#[fg=white]'
+    end
+
+
+    if test -n "$git_dir"
+        set project (string split '/' $git_dir)[-1]
+        set short_pwd (string replace $git_dir $project $PWD)
+
+        set branch (command git symbolic-ref --short --quiet HEAD)
+        set dirty (command git diff --no-ext-diff --ignore-submodules --quiet; echo $status)
+        set unmerged (command git cherry -v @\{upstream\} ^/dev/null)
+
+        if test $dirty -ne 0
+            set branch {$subtle_color}{$branch}
+        else
+            set branch {$strong_color}{$branch}
+        end
+
+        if test -n "$unmerged"
+            set branch $branch'⟳'
+        end
+
+        set prompt {$prompt}{$strong_color}{$short_pwd}{$normal_color}' on '{$branch}{$normal_color}
+    else
+        set short_pwd (string replace -r "^$HOME" '~' $PWD)
+        set prompt {$prompt}{$strong_color}{$short_pwd}{$normal_color}
+    end
+
+    if set -q TMUX
+        tmux set -q status-left $prompt
+    else
+        echo $prompt
+    end
 end
--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,12 +1,5 @@
 function fish_prompt --description 'Write out the prompt'
-    set -l last_status $status
-
-    set -l prompt_context_new (prompt_context)
-    if not test "$prompt_context_new" = "$__prompt_context_current"
-        set -g __prompt_context_current $prompt_context_new
-    end
-
-    if test $last_status -ne 0
+    if test $status -ne 0
         set_color $fish_color_error
     else
         set_color normal
