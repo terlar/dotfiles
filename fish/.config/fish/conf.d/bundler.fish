f4162ea307b57552186b4621bf8042787219c680
Terje Larsen <terlar@gmail.com>
2018-02-24 16:15:34 +0100

    fish: improve bundler bin-stub path pre-pend

--- fish/.config/fish/conf.d/bundler.fish
+++ fish/.config/fish/conf.d/bundler.fish
@@ -1,10 +1,24 @@
 # Toggle binstub path
-function __bundler_set_binstub_path --on-event fish_prompt
+function __bundler_set_binstub_path -v PWD
     set -l gemfile_path (file_in_path Gemfile)
 
-    if test -z $gemfile_path
-        __bundle_reset_binstub_path
+    test -e "$gemfile_path"
+    or return
+
+    set -l binstub_path (string replace /Gemfile '' $gemfile_path)/bin
+
+    if test -e "$binstub_path"
+        test "$__bundler_binpath" = "$binstub_path"
+        and return
+
+        set -g __bundler_binpath "$binstub_path"
+        set -g fish_user_paths $fish_user_paths $__bundler_binpath
     else
-        __bundle_set_binstub_path $gemfile_path
+        set -q __node_binpath
+        and set -l index (contains -i -- "$__bundler_binpath" $fish_user_paths)
+        and set -e fish_user_paths[$index]
+        and set -e __bundler_binpath
     end
 end
+
+__bundler_set_binstub_path $PWD
