4410caf270705c8b0e162e14e3f33af5548f6447
Terje Larsen <terlar@gmail.com>
2018-07-12 15:27:23 +0200

    fish: update = to use now math function
    Scale is now defined by -s argument

--- fish/.config/fish/functions/=.fish
+++ fish/.config/fish/functions/=.fish
@@ -2,5 +2,5 @@ function = --description 'Math wrapper'
     set calc $argv
     set calc (string replace xx '^' -- $calc)
     set calc (string replace x '*' -- $calc)
-    math "scale=10; $calc"
+    math -s10 "$calc"
 end
