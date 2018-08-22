7171db7dc7d8fe7c06ac1a5752c06fb1d9284a5d
Terje Larsen <terlar@gmail.com>
2018-08-22 17:37:50 +0200

    fish: improve find-src command
    Add completions and work with full path as suggested by the completion.

--- fish/.config/fish/functions/find-src.fish
+++ fish/.config/fish/functions/find-src.fish
@@ -1,11 +1,19 @@
 function find-src
     set -l root (ghq root)
-    set -l dir (ghq list $argv | fzf -0 -1)
-    set -l path $root/$dir
+    set -l path
 
-    if test -n $dir -a -d $path
+    if test -d "$root/$argv"
+        set path "$root/$argv"
+    else
+        set -l dir (ghq list "$argv" | fzf -0 -1)
+        if test -n "$dir" -a -d "$root/$dir"
+            set path "$root/$dir"
+        end
+    end
+
+    if test -n "$path"
         cd $path
     else
-        echo "find-src: no source directory matching $argv"
+        echo "find-src: no source directory matching: `$argv'"
     end
 end
