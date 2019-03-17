62a3a5ecc7848e6815d4b428f12c16444d8e4c63
Terje Larsen <terlar@gmail.com>
2019-03-17 19:47:02 +0100

    fish: fix print_duration

--- fish/.config/fish/functions/print_duration.fish
+++ fish/.config/fish/functions/print_duration.fish
@@ -1,15 +1,15 @@
 function print_duration -a duration
     if test "$duration" -gt 60000
-        set total_seconds (math $duration / 1000)
+        set total_seconds (math -s0 $duration / 1000)
         if test $total_seconds -gt 3600
-            set hours (math $total_seconds / 3600)
-            set remainder (math $total_seconds '%' 3600)
-            set minutes (math $remainder / 60)
-            set seconds (math $remainder '%' 60)
+            set hours (math -s0 $total_seconds / 3600)
+            set remainder (math -s0 $total_seconds '%' 3600)
+            set minutes (math -s0 $remainder / 60)
+            set seconds (math -s0 $remainder '%' 60)
             printf '(%dh%dm%ds)' $hours $minutes $seconds
         else if test $total_seconds -gt 60
-            set minutes (math $total_seconds / 60)
-            set seconds (math $total_seconds '%' 60)
+            set minutes (math -s0 $total_seconds / 60)
+            set seconds (math -s0 $total_seconds '%' 60)
             printf '(%dm%ds)' $minutes $seconds
         end
     else if test "$duration" -ge 100
