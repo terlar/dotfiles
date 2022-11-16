7aba7f992f77229fbf02a1a704c5683c5019a0fd
Terje Larsen <terlar@gmail.com>
2022-11-16 10:22:15 +0100

    fish: add support for prompt navigation

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,6 +6,11 @@ if test "$TERM" = eterm-color
     end
 end
 
+# Add marker to support navigation between prompts.
+function mark_prompt_start --on-event fish_prompt
+    echo -en "\e]133;A\e\\"
+end
+
 # Environment
 if type -q ev
     ev auto >/dev/null
