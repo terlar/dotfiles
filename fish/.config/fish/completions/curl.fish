b675c9079f41e72a6c536bf6ee3eea05235e72d3
Terje Larsen <terlar@gmail.com>
2022-11-17 01:00:37 +0100

    fish: add curl/curlie completions

---
+++ fish/.config/fish/completions/curl.fish
@@ -0,0 +1,6 @@
+complete --command curl \
+    --long-option request \
+    --short-option X \
+    --exclusive \
+    --description '(HTTP) Specifies a custom HTTP method' \
+    --arguments 'GET HEAD POST PUT DELETE CONNECT OPTIONS TRACE PATCH'
--- fish/.config/fish/completions/curlie.fish
+++ fish/.config/fish/completions/curlie.fish
@@ -1,1 +1,7 @@
+complete --command curlie \
+    --no-files \
+    --condition '__fish_is_nth_token 1' \
+    --description 'HTTP method' \
+    --arguments 'GET HEAD POST PUT DELETE CONNECT OPTIONS TRACE PATCH'
+
 complete --command curlie --wraps curl
